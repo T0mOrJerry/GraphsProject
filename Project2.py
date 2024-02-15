986861667e0b8dd6ab71c397bffe5b57a701bc7c
macbookalex <golovatyalex@mail.ru>
2024-02-15 17:56:28 +0000

    Final commit - everything is done

--- Project2.py
+++ Project2.py
@@ -12,10 +12,38 @@ class Graph:
             self.connections[node1].append(node2)
             self.connections[node2].append(node1)
 
+    def shortest_path(self, start, end):
+        visited = set()
+        queue = [(start, 0)]
+
+        while queue:
+            current_member, distance = queue.pop(0)
+
+            if current_member == end:
+                return distance
+
+            if current_member not in visited:
+                visited.add(current_member)
+                friends = self.connection[current_member]
+                queue.extend((friend, distance + 1) for friend in friends)
+
+        return 0
+
+    def is_connected(self):
+        x = len(self.connections)
+        key_con = list(self.connections.keys())
+        for i in key_con:
+            for j in key_con:
+                if i != j:
+                    if self.shortest_path(i, j) == 0:
+                        return "The graph it is not connected"
+        return "the graph is connected"
+
 
 network = Graph()
 # Add vertices and connect them
 network.add_vertex("A")
 network.add_vertex("B")
 network.add_edge("A", "B")
+network.is_connected()
 print('Done')
