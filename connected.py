0ecd54add58389c82b382638bf0399342c99f7f3
BestOne919 <157045772+BestOne919@users.noreply.github.com>
2024-02-15 20:27:32 +0300

    Update connected.py

--- connected.py
+++ connected.py
@@ -11,13 +11,31 @@ class Graph:
         else:
             self.connections[node1].append(node2)
             self.connections[node2].append(node1)
-        def is_connected(self, node):
-            int i=0
-            
-            z+=len(self.connection[i])
-            y=len(self.connection[i])
-            for(j in range y):
-                is_connected(self, self.connection[z][j])
+    def shortest_path(self, start, end):
+        visited = set()
+        queue = [(start, 0)]
+
+        while queue:
+            current_member, distance = queue.pop(0)
+
+            if current_member == end:
+                return distance
+
+            if current_member not in visited:
+                visited.add(current_member)
+                friends = self.connection[current_member]
+                queue.extend((friend, distance + 1) for friend in friends)
+
+        return 1.5
+    def is_connected(self):
+        x=len(self.connection)
+        for(i in range x):
+            for(j in range x):
+                if i != j:
+                    if shortest_path(self, self.connection[i], self.connection[j])==0:
+                        return False
+        return True
+                
             
         
 
